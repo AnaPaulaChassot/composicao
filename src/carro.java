678c8f1a5b600d523e227bff2aafb37165ab8a0a
Ana Paula Ferreira <anapchassot@hotmail.com>
2022-09-19 19:37:51 -0300

    composicao

---
+++ src/carro.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Suporte
+ */
+public class carro {
+    private motor m;
+    private direcao d;
+    
+    public motor getM() { // recupera
+        return m;
+    }
+
+    public void setM(motor m) { //grava
+        this.m = m;
+    }
+
+    public direcao getD() {
+        return d;
+    }
+
+    public void setD(direcao d) {
+        this.d = d;
+    }
+    
+    
+}
---
+++ src/motor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Suporte
+ */
+public class motor {
+    private int potencia;
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+    
+}
